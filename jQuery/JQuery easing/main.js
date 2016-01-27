5e79982676d843f65f1b0a62c7e6b5ef496d9965
Daniel Maldonado <danielwmaldonado@gmail.com>
2016-01-27 11:25:05 -0600

    initial commit
    easing practice w/ jQuery Easing Plugin by GSGD

---
+++ jQuery/JQuery easing/main.js
@@ -0,0 +1,21 @@
+﻿//<!--This tutorial is provided in part by Server Intellect Web Hosting Solutions http://www.serverintellect.com-->
+//<!--Visit http://www.JavaScriptatoms.com for more JavaScript Tutorials-->
+$(document).ready(function () {
+	"use strict"
+    var theBall = $('#ball');
+    var theSquare = $('#square');
+	var theButton = $("button");
+
+    theBall.click(function () {
+        $(this).animate({
+            top: '+=900'
+        }, 1500, 'easeOutBounce');
+    });
+
+    theButton.click(function () {
+        $(theSquare).animate({
+            height:"+=100",
+            width:"+=100",        
+        },1000,'easeOutElastic');
+    });
+});
