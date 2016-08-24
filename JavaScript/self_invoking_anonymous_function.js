23150afc6b807f14d1c354afd13d167ee07f564b
Daniel Maldonado <daniel@danielmdesigns.com>
2016-08-24 12:38:23 -0500

    init commit
    Heard a really good explanation (finally) on why’s of the self invoking
    anonymous function.

---
+++ JavaScript/self_invoking_anonymous_function.js
@@ -0,0 +1,13 @@
+// JavaScript Document
+
+
+//SELF INVOKING ANONYMOUS FUNCTION
+(function(){
+	//code here
+})();
+
+/*
+	BENEFITS:
+	1. It runs immediately runs on page load
+	2. Variables are local and NOT apart of the window object.
+*/
