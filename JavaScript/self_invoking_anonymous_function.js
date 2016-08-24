8736d2c873270c5f5df20fda729460863c2640ce
Daniel Maldonado <daniel@danielmdesigns.com>
2016-08-24 12:42:07 -0500

    jQuery example
    Another great explanation on this subject. Basically, when the elements
    event is triggered, run the function immediately.
    
    …so like I’ve known that, but IDK just took some time for me to really
    understand anon functions. ya, i know..

--- JavaScript/self_invoking_anonymous_function.js
+++ JavaScript/self_invoking_anonymous_function.js
@@ -11,3 +11,14 @@
 	1. It runs immediately runs on page load
 	2. Variables are local and NOT apart of the window object.
 */
+
+
+//ANOTHER EXAMPLE
+$("element").on("event", function(){
+	//code here
+});
+/*
+	HOW IT WORKS:
+	1. When the element event is triggered, run the function immdiately,
+	thus - running the code.
+*/
